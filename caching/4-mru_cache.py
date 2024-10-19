5a395b81baa86b446d789e6ef98cb9c65c784f65
Bryaurel <b.bakongobw@alustudent.com>
2024-10-19 02:04:04 +0200

    4-mru_cache.py

--- caching/4-mru_cache.py
+++ caching/4-mru_cache.py
@@ -8,52 +8,27 @@ from base_caching import BaseCaching
 
 
 class MRUCache(BaseCaching):
-    '''
-    MRUCache class implements an MRU caching system.
-    If the number of items in cache exceeds the limit,
-    it discards the most recently used item.
-    MRU: Most Recently Used
-    '''
-    def __init__(self):
-        '''
-        Initialize the cache and call the parent class initializer.
-        '''
-        super().__init__()
-        self.usage_order = []  # List to track access order
+    """ lru cache """
 
     def put(self, key, item):
-        '''
-        Add the item to the cache using the given key.
-        If the cache exceeds the MAX_ITEMS limit,
-        discard the most recently used item.
-        '''
+        """Add an item in the cache"""
         if key is None or item is None:
             return
-
-        # If key already exists, update its value and mark it as mru
+        if (key not in self.cache_data and
+                len(self.cache_data) >= BaseCaching.MAX_ITEMS):
+            if self.cache_data:
+                discard = next(reversed(list(self.cache_data)))
+                print("DISCARD: {}".format(discard))
+                del self.cache_data[discard]
         if key in self.cache_data:
-            self.usage_order.remove(key)
-
-        # Add the item to the cache
+            del self.cache_data[key]
         self.cache_data[key] = item
-        self.usage_order.append(key)
-
-        # If cache exceeds max limit, remove the MRU item
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            # Remove the last accessed (most recently used) item
-            mru_key = self.usage_order.pop(-1)
-            del self.cache_data[mru_key]
-            print(f"DISCARD: {mru_key}")
 
     def get(self, key):
-        '''
-        Retrieve the value from the cache for the given key.
-        Mark the key as most recently used.
-        '''
+        """Get an item in the cache"""
         if key is None or key not in self.cache_data:
             return None
-
-        # Move the key to the end to mark it as most recently used
-        self.usage_order.remove(key)
-        self.usage_order.append(key)
+        item = self.cache_data[key]
+        del self.cache_data[key]
+        self.cache_data[key] = item
         return self.cache_data[key]
